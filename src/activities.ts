06a6edc0a5858987395b606f972be6b348ab751a
Bob Reselman <bob@CogArtTech.com>
2023-03-14 15:34:33 -0700

    updating main

--- src/activities.ts
+++ src/activities.ts
@@ -1,69 +1,61 @@
-import {ApplicationFailure} from "@temporalio/workflow";
 import path from "path";
 import fs from "fs";
 
-const PAUSE_LENGTH= 4000;
-
+const dataFileSpec = path.join(__dirname, '../data', 'data.json')
 export async function getArticle(): Promise<string>  {
-    const dataFileSpec = path.join(__dirname, '../data', 'data.json')
     const data = fs.readFileSync(dataFileSpec,
         {encoding:'utf8', flag:'r'});
-    const articles = JSON.parse(data);
+    const articles = JSON.parse(data).articles;
     const randomIndex = Math.floor(Math.random() * articles.length);
     const randomEntry = articles[randomIndex];
-    await pause(PAUSE_LENGTH);
     return randomEntry;
 }
 
+export async function getEditor(): Promise<string>  {
+    const data = fs.readFileSync(dataFileSpec,
+        {encoding:'utf8', flag:'r'});
+    const articles = JSON.parse(data).editors;
+    const randomIndex = Math.floor(Math.random() * articles.length);
+    const randomEntry = articles[randomIndex];
+    return randomEntry;
+}
 
-export async function proofread(article:string): Promise<string> {
-    const msg = `I am proofreading [${article}].`;
+export async function proofread(editor: string, article:string): Promise<string> {
+    const msg = `${editor} is proofreading: ${article}.`;
 
     const arr : string[] = [];
     arr.push(msg);
-    arr.push(await checkSpelling(article));
-    arr.push(await checkGrammar(article));
+    arr.push(await checkSpelling(editor, article));
+    arr.push(await checkGrammar(editor, article));
     const json = JSON.stringify(arr,null, 2);
     console.log(json);
-    await pause(PAUSE_LENGTH);
     return json;
 }
 
-export async function copyEdit(article:string): Promise<string> {
-    const msg = `I am copy editing [${article}].`;
+export async function copyEdit(editor: string, article:string): Promise<string> {
+    const msg = `${editor} is copy editing: ${article}.`;
     console.log(msg);
-    await pause(PAUSE_LENGTH);
     return msg;
 }
 
-export async function formatEdit(article:string): Promise<string> {
-    const msg = `I am format editing [${article}].`;
+export async function formatEdit(editor: string, article:string): Promise<string> {
+    const msg = `${editor} is format editing: ${article}.`;
     console.log(msg);
-    await pause(PAUSE_LENGTH);
     return msg;
 }
 
-export async function techEdit(article:string): Promise<string> {
-    const msg = `I am tech editing [${article}].`;
+export async function techEdit(editor: string, article:string): Promise<string> {
+    const msg = `${editor} is tech editing: ${article}.`;
     console.log(msg);
-    await pause(PAUSE_LENGTH);
     return msg;
 }
 
-export async function checkSpelling(article:string): Promise<string> {
-    await pause(PAUSE_LENGTH);
-    return `I am spell checking [${article}].`;
+export async function checkSpelling(editor: string, article:string): Promise<string> {
+    const msg = `${editor} is spell checking: ${article}.`;
+    return msg;
 }
 
-export async function checkGrammar(article:string): Promise<string> {
-    await pause(PAUSE_LENGTH);
-    return `I am grammar checking [${article}].`;
+export async function checkGrammar(editor: string, article:string): Promise<string> {
+    const msg = `${editor} is grammar checking: ${article}.`;
+    return msg;
 }
-
-
-async function pause(milliseconds: number): Promise<void> {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
-}
-
-
-
--- src/workflows.ts
+++ src/workflows.ts
@@ -2,12 +2,10 @@ import * as wf from '@temporalio/workflow';
 // Only import the activity types
 import type * as activities from './activities';
 
-
-
 const maximumAttempts = 10; //The number of times to retry
 
-// Get the activities function in order to make them to the workflow.
-const {getArticle, proofread, copyEdit, techEdit, formatEdit} = wf.proxyActivities<typeof activities>({
+// Get the activities function in order to make them available to the workflow.
+const {getArticle, getEditor, proofread, copyEdit, techEdit, formatEdit} = wf.proxyActivities<typeof activities>({
     //More info about startToCloseTimeout is here: https://docs.temporal.io/concepts/what-is-a-start-to-close-timeout/
     startToCloseTimeout: '4 seconds',
     retry: {
@@ -20,13 +18,13 @@ export async function techPublishingWorkflow(): Promise<void> {
     const startTime = new Date(Date.now()).toString();
     const article = await getArticle();
 
-    const pr = await proofread(article);
+    const pr = await proofread(await getEditor(), article);
 
-    const te = await techEdit(article);
+    const te = await techEdit(await getEditor(), article);
 
-    const ce = await copyEdit(article);
+    const ce = await copyEdit(await getEditor(), article);
 
-    const fe = await formatEdit(article);
+    const fe = await formatEdit(await getEditor(), article);
 
     const endTime = new Date(Date.now()).toString();
 
@@ -41,5 +39,3 @@ export async function techPublishingWorkflow(): Promise<void> {
 
     console.log(JSON.stringify(techPub,null, 2));
 }
-
-
