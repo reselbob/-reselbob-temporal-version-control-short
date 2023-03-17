53d03f68e9299070beb7ef01eceb1f4755d20248
Bob Reselman <bob@CogArtTech.com>
2023-03-17 15:10:05 -0700

    applying patch

--- src/workflows.ts
+++ src/workflows.ts
@@ -1,6 +1,7 @@
 import * as wf from '@temporalio/workflow';
 // Only import the activity types
 import type * as activities from './activities';
+import { patched } from '@temporalio/workflow';
 
 // Get the activities function in order to make them to the workflow.
 const {getArticle, getEditor, proofread, copyEdit, techEdit, formatEdit, getBrandingApproval} = wf.proxyActivities<typeof activities>({
@@ -13,33 +14,56 @@ const {getArticle, getEditor, proofread, copyEdit, techEdit, formatEdit, getBran
 });
 
 export async function techPublishingWorkflow(): Promise<void> {
+
     const startTime = new Date(Date.now()).toString();
     const article = await getArticle();
+    let techPub = {};
+    if (patched('V3')) {
+        const te = await techEdit(await getEditor(), article);
 
-    const te = await techEdit(await getEditor(), article);
+        const pr = await proofread(await getEditor(), article);
 
-    const pr = await proofread(await getEditor(), article);
+        const ce = await copyEdit(await getEditor(), article);
 
-    const ce = await copyEdit(await getEditor(), article);
+        const fe = await formatEdit(await getEditor(), article);
 
-    const fe = await formatEdit(await getEditor(), article);
+        const ba = await getBrandingApproval(article);
 
-    const ba = await getBrandingApproval(article);
+        if(!ba){
+            console.log(`${article} did not get through branding`)
+        }
+        techPub = {
+            startTime,
+            techEdit: te,
+            proofread: pr,
+            copyEdit: ce,
+            formatEdit: fe,
+            brandingApproval: ba,
+            endTime:  new Date(Date.now()).toString()
+        }
+    }else {
+        const te = await techEdit(await getEditor(), article);
 
-    if(!ba){
-        console.log(`${article} did not get through branding`)
-    }
+        const pr = await proofread(await getEditor(), article);
 
-    const endTime = new Date(Date.now()).toString();
+        const ce = await copyEdit(await getEditor(), article);
 
-    const techPub = {
-        startTime,
-        techEdit: te,
-        proofread: pr,
-        copyEdit: ce,
-        formatEdit: fe,
-        brandingApproval: ba,
-        endTime
+        const fe = await formatEdit(await getEditor(), article);
+
+        const ba = await getBrandingApproval(article);
+
+        if(!ba){
+            console.log(`${article} did not get through branding`)
+        }
+        techPub = {
+            startTime,
+            techEdit: te,
+            proofread: pr,
+            copyEdit: ce,
+            formatEdit: fe,
+            brandingApproval: ba,
+            endTime: new Date(Date.now()).toString()
+        }
     }
 
     console.log(JSON.stringify(techPub,null, 2));
