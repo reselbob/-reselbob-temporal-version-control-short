8ae97fa47ba673e371c9f97c1d2b9c4d301fd7db
Bob Reselman <bob@CogArtTech.com>
2023-03-30 18:02:09 -0700

    changing timing and code

--- src/workflows.ts
+++ src/workflows.ts
@@ -23,81 +23,77 @@ const {
 export async function techPublishingWorkflow(publisher: string): Promise<void> {
     const startTime = new Date(Date.now()).toString();
     const article = await getArticle();
-    let editor='';
+    let editor = '';
+    let v = '';
+    let pr = '';
+    let te = '';
+    let ce = ''
+    let fe = '';
     let techPub = {};
-    if(wf.patched('V4')) {
-        editor = await getEditor();
-        const te = await techEdit_v4({editor, article, publisher});
+    if (wf.patched('V4')) {
+        v = 'V4';
 
+        editor = await getEditor();
+        te = await techEdit_v4({editor, article, publisher});
         //             millisec * sec * min
         await wf.sleep(1000 * 60 * 5);
         editor = await getEditor();
-        const pr = await proofread_v4({editor, article, publisher});
+        pr = await proofread_v4({editor, article, publisher});
 
         editor = await getEditor();
-        const ce = await copyEdit_v4({editor, article, publisher});
+        ce = await copyEdit_v4({editor, article, publisher});
 
         editor = await getEditor();
-        const fe = await formatEdit_v4({editor, article, publisher});
+        fe = await formatEdit_v4({editor, article, publisher});
 
-        const endTime = new Date(Date.now()).toString();
-
-        techPub = {
-            startTime,
-            techEdit: te,
-            proofread: pr,
-            copyEdit: ce,
-            formatEdit: fe,
-            endTime
-        }
     }
-    else if((wf.patched('V3')) ){
-        let editor=await getEditor();
-        const te = await techEdit(editor, article);
+    if (wf.patched('V3')) {
+        v = 'V3';
+        te = await techEdit(await getEditor(), article);
 
-        editor=await getEditor();
-        const pr = await proofread(editor, article);
+        //             millisec * sec * min
+        await wf.sleep(1000 * 60 * 5);
+        pr = await proofread(await getEditor(), article);
 
-        editor=await getEditor();
-        const ce = await copyEdit(editor, article);
+        ce = await copyEdit(await getEditor(), article);
 
-        editor=await getEditor();
-        const fe = await formatEdit(editor, article);
-
-        editor=await getEditor();
-
-        techPub = {
-            startTime,
-            techEdit: te,
-            proofread: pr,
-            copyEdit: ce,
-            formatEdit: fe,
-            endTime:  new Date(Date.now()).toString()
-        }
-    }
-    else {
-        let editor=await getEditor();
-
-        const pr = await proofread(editor, article);
-
-        editor=await getEditor();
-        const te = await techEdit(editor, article);
-
-        editor=await getEditor();
-        const ce = await copyEdit(editor, article);
-
-        editor=await getEditor();
-        const fe = await formatEdit(editor, article);
-
-        techPub = {
-            startTime,
-            proofread: pr,
-            techEdit: te,
-            copyEdit: ce,
-            formatEdit: fe,
-            endTime: new Date(Date.now()).toString()
-        }
+        fe = await formatEdit(await getEditor(), article);
     }
 
-    console.log(JSON.stringify(techPub,null, 2));
+    if (wf.patched('V2')) {
+        v = 'V2';
+        pr = await proofread(await getEditor(), article);
+        //             millisec * sec * min
+        await wf.sleep(1000 * 60 * 5);
+        te = await techEdit(await getEditor(), article);
+
+        ce = await copyEdit(await getEditor(), article);
+
+        fe = await formatEdit(await getEditor(), article);
+    }
+
+    if (wf.patched('V1')) {
+        v = 'V1';
+        pr = await proofread(await getEditor(), article);
+        //             millisec * sec * min
+        await wf.sleep(1000 * 60 * 5);
+        te = await techEdit(await getEditor(), article);
+
+        ce = await copyEdit(await getEditor(), article);
+
+        fe = await formatEdit(await getEditor(), article);
+    }
+
+    const endTime = new Date(Date.now()).toString();
+
+    techPub = {
+        startTime,
+        techEdit: te,
+        proofread: pr,
+        copyEdit: ce,
+        formatEdit: fe,
+        endTime
+    }
+
+    console.log(JSON.stringify(techPub, null, 2));
 }
