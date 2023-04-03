7b5a01a549d18e76a98ed647633cf3c7f3c58c29
Bob Reselman <bob@CogArtTech.com>
2023-04-02 17:01:08 -0700

    changing timespans

--- src/workflows.ts
+++ src/workflows.ts
@@ -16,7 +16,8 @@ NOTE: The workflow has a delay between the first and second activity
  */
 export async function techPublishingWorkflow(): Promise<void> {
     //                  millisec * sec * min
-    const sleepPeriod = (1000 * 60 * 5)
+    const sleepPeriod01 = (1000 * 60 * 3)
+    const sleepPeriod02 = (1000 * 60 * 1)
 
     const startTime = new Date(Date.now()).toString();
     const article = await getArticle();
@@ -31,9 +32,11 @@ export async function techPublishingWorkflow(): Promise<void> {
         v = 'Release_activity_reorder';
         te = await techEdit(await getEditor(), article);
 
-        await wf.sleep(sleepPeriod);
+        await wf.sleep(sleepPeriod01);
 
         pr = await proofread(await getEditor(), article);
+
+        await wf.sleep(sleepPeriod02);
 
         ce = await copyEdit(await getEditor(), article);
 
@@ -42,9 +45,11 @@ export async function techPublishingWorkflow(): Promise<void> {
         v = 'Release_original';
         pr = await proofread(await getEditor(), article);
 
-        await wf.sleep(sleepPeriod);
+        await wf.sleep(sleepPeriod01);
 
         te = await techEdit(await getEditor(), article);
+
+        await wf.sleep(sleepPeriod02);
 
         ce = await copyEdit(await getEditor(), article);
 
