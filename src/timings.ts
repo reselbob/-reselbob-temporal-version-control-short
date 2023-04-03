701365af678d753bff7d52d629521747d815a60e
Bob Reselman <bob@CogArtTech.com>
2023-04-02 18:05:28 -0700

    Update timings.ts

--- src/timings.ts
+++ src/timings.ts
@@ -1,5 +1,5 @@
 export const timings = {
-    cronSchedule: '*/4 * * * *',
+    cronSchedule: '*/2 * * * *',
     //            millisec * sec * min
     sleepPeriod01: (1000 * 60 * 3),
     sleepPeriod02: (1000 * 60 * 1)
