2d153c3bd4ce77facaa1b470cb4d41330b9dca78
Bob Reselman <bob@CogArtTech.com>
2023-04-08 12:04:29 -0700

    adding shell script

---
+++ autoexecutewf.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+git clone https://github.com/reselbob/temporal-version-control-01.git
+
+cd temporal-version-control
+
+git checkout v1-error
+
+npm install
+
+ts-node src/worker.ts &
+
+ts-node src/client.ts
+
+PROC_ID=$(ps -aux | grep "sh \-c ts-node src/worker.ts" | awk '{print $2}')
+
+echo $PROC_ID
+
+kill PROC_ID
+
+
+sleep 150
+
+echo "Hi 2"
+
+sleep 150
+
+echo "Hi 3"
+
+sleep 150
+
+echo "Hi 4"
+
+sleep 150
+
+echo "Hi 5"
