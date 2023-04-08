3447b2774ad5f277801486c338033c92eb2bac09
Bob Reselman <bob@CogArtTech.com>
2023-04-08 13:07:12 -0700

    adding shell script

--- autoexecutewf.sh
+++ autoexecutewf.sh
@@ -4,29 +4,77 @@ git checkout v1-error
 
 npm install
 
-ts-node src/worker.ts
+P_ID=$(nohup npm start > output1.log 2>&1 & echo $!)
 
-ts-node src/client.ts
+V1_ERR_ID=$(nohup npm run workflow-from-client > output2.log 2>&1 & echo $!)
 
-PROC_ID=$(ps -aux | grep "sh \-c ts-node src/worker.ts" | awk '{print $2}')
+echo "P_ID is $P_ID"
 
-echo $PROC_ID
+echo "V1_ERR_ID is $V1_ERR_ID"
 
-# kill PROC_ID
+# V1
+sleep 150
 
+echo "P_ID is $P_ID"
+
+kill $P_ID
+
+git checkout V1
+
+V1_ID=$(nohup npm run workflow-from-client > output2.log 2>&1 & echo $!)
+
+P_ID=$(nohup npm start > output1.log 2>&1 & echo $!)
+
+echo "P_ID is $P_ID"
+
+echo "V1_ID is $V1_ID"
+
+
+# V2
+sleep 150
+
+kill $P_ID
+
+git checkout V2
+
+V2_ID=$(nohup npm run workflow-from-client > output2.log 2>&1 & echo $!)
+
+P_ID=$(nohup npm start > output1.log 2>&1 & echo $!)
+
+echo "P_ID is $P_ID"
+
+echo "V2_ID is $V2_ID"
+
+# V3
 
 sleep 150
 
-echo "Hi 2"
+kill $P_ID
+
+git checkout V3
+
+V3_ID=$(nohup npm run workflow-from-client > output2.log 2>&1 & echo $!)
+
+P_ID=$(nohup npm start > output1.log 2>&1 & echo $!)
+
+echo "P_ID is $P_ID"
+
+echo "V3_ID is $V3_ID"
+
+# V4
 
 sleep 150
 
-echo "Hi 3"
+kill $P_ID
 
-sleep 150
+git checkout V4
 
-echo "Hi 4"
+V4_ID=$(nohup npm run workflow-from-client > output2.log 2>&1 & echo $!)
 
-sleep 150
+P_ID=$(nohup npm start > output1.log 2>&1 & echo $!)
 
-echo "Hi 5"
+echo "P_ID is $P_ID"
+
+echo "V4_ID is $V4_ID"
+
+echo "All versions release"
