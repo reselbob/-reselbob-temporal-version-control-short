a02dfb944f8127a022c3ce455b2d72415b0e850e
Bob Reselman <bob@CogArtTech.com>
2023-04-10 12:31:26 -0700

    changing close to timeout

--- src/workflows.ts
+++ src/workflows.ts
@@ -3,7 +3,7 @@ import type * as activities from './activities';
 import {timings} from "./timings";
 const {getArticle, getEditor, proofread, copyEdit, techEdit, formatEdit} = wf.proxyActivities<typeof activities>({
     //More info about startToCloseTimeout is here: https://docs.temporal.io/concepts/what-is-a-start-to-close-timeout/
-    startToCloseTimeout: '6m',
+    startToCloseTimeout: '60 seconds',
     retry: {
         backoffCoefficient: 1,
         maximumAttempts: 2,
