05f2b48b66dcfe48e880ee08353017db763cab15
Bob Reselman <bob@CogArtTech.com>
2023-04-10 09:59:00 -0700

    changing start to close interval

--- src/workflows.ts
+++ src/workflows.ts
@@ -3,7 +3,7 @@ import type * as activities from './activities';
 import {timings} from "./timings";
 
 const {getArticle, getEditor, proofread, copyEdit, techEdit, formatEdit} = wf.proxyActivities<typeof activities>({
-    startToCloseTimeout: '4 seconds',
+    startToCloseTimeout: '6m',
     retry: {
         backoffCoefficient: 1,
         maximumAttempts: 2,
