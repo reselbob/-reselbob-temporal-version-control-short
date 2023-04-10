229e00f7b3fd74b81631a215a09832057c520b4d
Bob Reselman <bob@CogArtTech.com>
2023-04-10 16:25:53 -0700

    revising script

--- autoexecutewf.sh
+++ autoexecutewf.sh
@@ -11,11 +11,11 @@ echo $RELEASE_MESSAGE
 
 echo "Firing off V1-error at $(date +"%Y-%m-%d %H:%M:%S")"
 
-P_ID=$(nohup npm start >> worker.log 2>&1 & echo $!)
+WORKER_ID=$(nohup ts-node src/worker.ts >> worker.log 2>&1 & echo $!)
 
-V1_ERR_ID=$(nohup npm run workflow-from-client > outputv1-error.log 2>&1 & echo $!)
+V1_ERR_ID=$(nohup ts-node src/client.ts > outputv1-error.log 2>&1 & echo $!)
 
-echo "P_ID is $P_ID"
+echo "WORKER_ID is $WORKER_ID"
 
 echo "V1_ERR_ID is $V1_ERR_ID"
 
@@ -24,19 +24,19 @@ echo "Waiting ... on V1\n\n"
 # V1
 sleep $SLEEP_PERIOD
 
-echo "Killing P_ID $P_ID and subprocesses"
+echo "Killing WORKER_ID $WORKER_ID and subprocesses"
 
-kill $(ps -s $$ -o pid=$P_ID)
+kill $WORKER_ID
 
 git checkout V1
 
 echo "Firing off V1 at $(date +"%Y-%m-%d %H:%M:%S")"
 
-V1_ID=$(nohup npm run workflow-from-client > outputv1.log 2>&1 & echo $!)
+V1_ID=$(nohup ts-node src/client.ts > outputv1.log 2>&1 & echo $!)
 
-P_ID=$(nohup npm start >> worker.log 2>&1 & echo $!)
+WORKER_ID=$(nohup ts-node src/worker.ts >> worker.log 2>&1 & echo $!)
 
-echo "P_ID is $P_ID"
+echo "WORKER_ID is $WORKER_ID"
 
 echo "V1_ID is $V1_ID"
 
@@ -45,19 +45,19 @@ echo "Waiting ... on V2\n\n"
 
 sleep $SLEEP_PERIOD
 
-echo "Killing P_ID $P_ID and subprocesses"
+echo "Killing WORKER_ID $WORKER_ID and subprocesses"
 
-kill $(ps -s $$ -o pid=$P_ID)
+kill $WORKER_ID
 
 git checkout V2
 
 echo "Firing off V2 at $(date +"%Y-%m-%d %H:%M:%S")"
 
-V2_ID=$(nohup npm run workflow-from-client > outputv2.log 2>&1 & echo $!)
+V2_ID=$(nohup ts-node src/client.ts > outputv2.log 2>&1 & echo $!)
 
-P_ID=$(nohup npm start >> worker.log 2>&1 & echo $!)
+WORKER_ID=$(nohup ts-node src/worker.ts >> worker.log 2>&1 & echo $!)
 
-echo "P_ID is $P_ID"
+echo "WORKER_ID is $WORKER_ID"
 
 echo "V2_ID is $V2_ID"
 
@@ -67,19 +67,19 @@ echo "Waiting ... on V3\n\n"
 
 sleep $SLEEP_PERIOD
 
-echo "Killing P_ID $P_ID and subprocesses"
+echo "Killing WORKER_ID $WORKER_ID and subprocesses"
 
-kill $(ps -s $$ -o pid=$P_ID)
+kill $WORKER_ID
 
 git checkout V3
 
 echo "Firing off V3 at $(date +"%Y-%m-%d %H:%M:%S")"
 
-V3_ID=$(nohup npm run workflow-from-client > output-v3.log 2>&1 & echo $!)
+V3_ID=$(nohup ts-node src/client.ts > output-v3.log 2>&1 & echo $!)
 
-P_ID=$(nohup npm start >> worker.log 2>&1 & echo $!)
+WORKER_ID=$(nohup ts-node src/worker.ts >> worker.log 2>&1 & echo $!)
 
-echo "P_ID is $P_ID"
+echo "WORKER_ID is $WORKER_ID"
 
 echo "V3_ID is $V3_ID"
 
@@ -89,19 +89,19 @@ echo "Waiting ... on V4\n\n"
 
 sleep $SLEEP_PERIOD
 
-echo "Killing P_ID $P_ID and subprocesses"
+echo "Killing WORKER_ID $WORKER_ID and subprocesses"
 
-kill $(ps -s $$ -o pid=$P_ID)
+kill $WORKER_ID
 
 git checkout V4
 
 echo "Firing off V4 at $(date +"%Y-%m-%d %H:%M:%S")"
 
-V4_ID=$(nohup npm run workflow-from-client > output-v4.log 2>&1 & echo $!)
+V4_ID=$(nohup ts-node src/client.ts > output-v4.log 2>&1 & echo $!)
 
-P_ID=$(nohup npm start >> worker.log 2>&1 & echo $!)
+WORKER_ID=$(nohup ts-node src/worker.ts >> worker.log 2>&1 & echo $!)
 
-echo "P_ID is $P_ID"
+echo "WORKER_ID is $WORKER_ID"
 
 echo "V4_ID is $V4_ID"
 
