bd85c489a3106d3cc27a1018c584ddc377fad0a8
Bob Reselman <bob@CogArtTech.com>
2023-04-10 11:57:56 -0700

    resetting run interval

--- autoexecutewf.sh
+++ autoexecutewf.sh
@@ -3,7 +3,7 @@ npm install
 
 git checkout V1-error
 
-SLEEP_PERIOD=190
+SLEEP_PERIOD=360
 
 RELEASE_MESSAGE="I will release a new version of the demonstration project every $SLEEP_PERIOD seconds"
 
