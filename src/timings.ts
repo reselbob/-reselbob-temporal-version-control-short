805935252638bf14792577a180fb662b87c0b8c6
Bob Reselman <bob@CogArtTech.com>
2023-05-04 09:13:14 -0700

    changing cronSchedule to 10 minutes

--- src/timings.ts
+++ src/timings.ts
@@ -1,5 +1,6 @@
 export const timings = {
-    cronSchedule: '*/2 * * * *',
+    // run the job every 10 minutes
+    cronSchedule: '*/10 * * * *',
     //            millisec * sec * min
     sleepPeriod01: (1000 * 60 * 3),
     sleepPeriod02: (1000 * 60 * 1)
